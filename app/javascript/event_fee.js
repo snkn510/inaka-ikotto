b85cdb901c86690d5d375cf15cc4f83a77d2231f
snkn510 <1192seina@gmail.com>
2023-06-21 14:38:06 +0900

    合計金額自動表示

--- app/javascript/event_fee.js
+++ app/javascript/event_fee.js
@@ -1,4 +1,19 @@
 window.addEventListener('load', () => {
-  const priceInput = document.getElementById("event_fee");
-console.log(priceInput);
+  const eventPriceElement = document.querySelector("[data-event-price]");
+  const eventPrice = parseInt(eventPriceElement.dataset.eventPrice);
+  const adultNumberInput = document.getElementById("adult_number");
+  const childNumberInput = document.getElementById("child_number");
+  const sumFeeElement = document.getElementById("sum_fee");
+
+  const calculateTotalFee = () => {
+    const adultNumber = parseInt(adultNumberInput.value) || 0;
+    const childNumber = parseInt(childNumberInput.value) || 0;
+
+    const totalFee = (adultNumber + childNumber) * eventPrice;
+
+    sumFeeElement.textContent = totalFee;
+  };
+
+  adultNumberInput.addEventListener("input", calculateTotalFee);
+  childNumberInput.addEventListener("input", calculateTotalFee);
 });
